e763dd21bb1b749b0f3fcc9ed23268522ad03e35
LangatD <dorcaschepngetich20@gmail.com>
2025-02-04 22:24:34 +0300

    cors fixed1

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from sqlalchemy.engine import URL
 from datetime import timedelta
+from sqlalchemy import create_engine
 load_dotenv()
 
 class Config:
@@ -22,3 +23,6 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "your-jwt-secret-key")
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=45)
+    DATABASE_URL="postgresql://postgres.jnprlgdommvwvtcoyfsj:kipkoech2024@aws-0-eu-central-1.pooler.supabase.com:5432/postgres?sslmode=verify-full"
+    engine = create_engine(DATABASE_URL)
+    
